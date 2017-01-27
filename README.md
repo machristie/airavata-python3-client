7e0349821aba6437284373fb8576503904532d83
Marcus Christie <machrist@iu.edu>
2017-01-27 13:47:40 -0500

    logging into identity server

---
+++ README.md
@@ -0,0 +1,52 @@
+
+# Running
+
+* copy airavata-client.ini.template to airavata-client.ini and change properties
+* install Python 3
+* create a virtual environment and activate it
+      python3 -m venv ENV
+      . ENV/bin/activate
+* install dependencies
+      pip install -r requirements.txt
+
+# Generating Thrift Stubs
+
+The thrift stubs are committed into the repo. These notes are on how I
+generated the thrift stubs and can be useful if needing to regenerate them.
+
+Thrift 0.10.0 is required to generate Python 3 compatible Thrift stubs.
+
+I decided to use Docker to run Thrift 0.10.0, but that isn't necessary. To
+use Docker for Thrift
+* download [Thrift 0.10.0 tarball](https://thrift.apache.org/) and untar it
+* in thrift-0.10.0 run the following
+      docker build -t thrift-0.10.0 .
+* checkout [airavata](https://git-wip-us.apache.org/repos/asf?p=airavata.git) and run thrift
+      cd airavata/thrift-interface-descriptions
+      mkdir target
+      docker run --rm -v "$PWD:/data" thrift-0.10.0 --gen py -r -o /data/target /data/airavata-apis/airavata_api.thrift
+* then copy the generated files into the current directory
+      cp -r target/gen-py/ /path/to/this/repo
+
+## Error: DataReplicaLocationModel is not defined
+
+      (ENV) BL-UITS-RTLT021:airavata-python3 machrist$ python test.py 
+      Traceback (most recent call last):
+        File "test.py", line 2, in <module>
+          from apache.airavata.api import Airavata
+        File "/Users/machrist/Documents/Airavata/airavata-python3/apache/airavata/api/Airavata.py", line 13, in <module>
+          from .ttypes import *
+        File "/Users/machrist/Documents/Airavata/airavata-python3/apache/airavata/api/ttypes.py", line 14, in <module>
+          import apache.airavata.model.ttypes
+        File "/Users/machrist/Documents/Airavata/airavata-python3/apache/airavata/model/ttypes.py", line 24, in <module>
+          import apache.airavata.model.data.replica.ttypes
+        File "/Users/machrist/Documents/Airavata/airavata-python3/apache/airavata/model/data/replica/ttypes.py", line 67, in <module>
+          class DataProductModel(object):
+        File "/Users/machrist/Documents/Airavata/airavata-python3/apache/airavata/model/data/replica/ttypes.py", line 97, in DataProductModel
+          (12, TType.LIST, 'replicaLocations', (TType.STRUCT, (DataReplicaLocationModel, DataReplicaLocationModel.thrift_spec), False), None, ),  # 12
+      NameError: name 'DataReplicaLocationModel' is not defined
+
+Problem is that DataReplicaLocationModel is defined after DataProductModel
+
+Solution was to reorder the generated Python code.
+
--- test.py
+++ test.py
@@ -4,38 +4,70 @@ from apache.airavata.api.ttypes import *
 from apache.airavata.model.workspace.ttypes import *
 from apache.airavata.model.security.ttypes import AuthzToken
 
+import requests
+
 from thrift import Thrift
 from thrift.transport import TSSLSocket
 from thrift.transport import TSocket
 from thrift.transport import TTransport
 from thrift.protocol import TBinaryProtocol
 
-try:
+import configparser
+
+def get_transport(hostname, port):
     # Create a socket to the Airavata Server
     # TODO: validate server certificate
-    transport = TSSLSocket.TSSLSocket('gw56.iu.xsede.org', 9930, validate=False)
-    # transport = TSocket.TSocket('gw56.iu.xsede.org', 9930)
+    transport = TSSLSocket.TSSLSocket(hostname, port, validate=False)
 
     # Use Buffered Protocol to speedup over raw sockets
     transport = TTransport.TBufferedTransport(transport)
+    return transport
 
+def get_airavata_client(transport):
     # Airavata currently uses Binary Protocol
     protocol = TBinaryProtocol.TBinaryProtocol(transport)
 
     # Create a Airavata client to use the protocol encoder
     airavataClient = Airavata.Client(protocol)
+    return airavataClient
 
-    # Connect to Airavata Server
+def get_authz_token(accessTokenURL, userInfoURL, clientKey, clientSecret, username, password, gatewayID):
+    
+    request_data = {'grant_type': 'password', 'scope': 'openid', 'username': username, 'password': password}
+    access_token_req = requests.post(accessTokenURL, auth=(clientKey, clientSecret), data=request_data, verify=False)
+    access_token = access_token_req.json()['access_token']
+    user_info_req = requests.get(userInfoURL, headers={'Authorization': "Bearer " + access_token}, verify=False)
+    
+    return AuthzToken(accessToken=access_token, claimsMap={'gatewayID': gatewayID, 'userName': user_info_req.json()['sub']})
+
+def get_all_projects(airavataClient, authzToken, gatewayId, username):
+
+    projectLists = airavataClient.getUserProjects(authzToken, gatewayId, username, -1, 0)
+
+    return projectLists
+
+if __name__ == '__main__':
+    
+    config = configparser.ConfigParser()
+    config.read('airavata-client.ini')
+    accessTokenURL = config['identity-server']['AccessTokenURL']
+    userInfoURL = config['identity-server']['UserInfoURL']
+    clientKey = config['identity-server']['ClientKey']
+    clientSecret = config['identity-server']['ClientSecret']
+    tenantDomain = config['identity-server']['TenantDomain']
+    
+    username = config['credentials']['Username']
+    password = config['credentials']['Password']
+    gatewayID = config['credentials']['GatewayID']
+    authz_token = get_authz_token(accessTokenURL, userInfoURL, clientKey, clientSecret, username + "@" + tenantDomain, password, gatewayID)
+    print(authz_token)
+    
+    hostname = config['airavata-api-server']['Hostname']
+    port = config['airavata-api-server']['Port']
+    transport = get_transport(hostname, port)
     transport.open()
-
-    projectLists = airavataClient.getUserProjects(AuthzToken("dummy-token"), "seagrid", "marcus-user", -1, 0);
-
-    print(projectLists)
-
-    # Close Connection to Airavata Server
+    airavataClient = get_airavata_client(transport)
+    
+    projects = get_all_projects(airavataClient, authz_token, gatewayID, username)
     transport.close()
-
-except Thrift.TException as tx:
-    print(tx.message)
-    raise tx
-
+    print(projects)
